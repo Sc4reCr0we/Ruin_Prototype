135b0a10afb5eb010f908f40ba6278d8a31bb713
DennisKrone <dennis.krone90@gmail.com>
2014-06-20 13:35:47 +0200

    Lagt till fmod banken. Vissa placeholders sitter på spells

---
+++ Assets/Scripts/Sound/PlayOneShot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOneShot : MonoBehaviour {
+
+	public FMODAsset SoundToBePlayed = null;
+	public bool PlayOnAwake = true;
+
+	// Use this for initialization
+	void Start () {
+
+	if(PlayOnAwake)
+			FMOD_StudioSystem.instance.PlayOneShot(SoundToBePlayed, transform.position);
+
+	}
+	
+
+}
