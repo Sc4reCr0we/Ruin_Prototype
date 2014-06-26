2a63a53be94e8616eb6c29a51053de0ef93bd23e
Martin Eriksson <haywer@gmail.com>
2014-06-26 11:32:31 +0200

    added particle effects

---
+++ Assets/Scripts/sortingLayer1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sortingLayer1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.sortingLayerName = "Foreground";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
